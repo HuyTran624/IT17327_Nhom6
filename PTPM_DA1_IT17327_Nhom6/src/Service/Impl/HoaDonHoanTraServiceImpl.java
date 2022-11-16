756fd705af54f47af0a37045292ab222d9cb3191
kitsuneremi <89727355+kitsuneremi@users.noreply.github.com>
2022-11-17 00:31:32 +0700

    x

---
+++ PTPM_DA1_IT17327_Nhom6/src/Service/Impl/HoaDonHoanTraServiceImpl.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Service.Impl;
+
+import Service.HoaDonHoanTraService;
+
+/**
+ *
+ * @author lilyp
+ */
+public class HoaDonHoanTraServiceImpl implements HoaDonHoanTraService{
+    
+}
