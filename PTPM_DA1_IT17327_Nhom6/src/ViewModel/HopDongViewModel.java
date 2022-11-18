46b1e60b4643f9f8bdb519e206a41d7dcfa27586
kitsuneremi <89727355+kitsuneremi@users.noreply.github.com>
2022-11-18 12:49:50 +0700

    1811

--- PTPM_DA1_IT17327_Nhom6/src/ViewModel/HopDongViewModel.java
+++ PTPM_DA1_IT17327_Nhom6/src/ViewModel/HopDongViewModel.java
@@ -4,10 +4,8 @@
  */
 package ViewModel;
 
-<<<<<<< HEAD
 import Model.*;
-=======
->>>>>>> d8f254dd8ea954ed9152f2843ee961bd2c05585a
+
 import java.util.Date;
 
 /**
@@ -15,43 +13,23 @@ import java.util.Date;
  * @author User
  */
 public class HopDongViewModel {
-<<<<<<< HEAD
+
     private String id;
     private String idnv;
     private String idkh;
     private Date ngayTao;
     private Date ngayHetHan;
     private int tinhTrang;
-=======
-        private String id;
-        private String idnv;
-        private String idkh;
-        private Date ngaytao;
-        private Date ngayhethan;
-        private int tinhTrangXe;
-        
->>>>>>> d8f254dd8ea954ed9152f2843ee961bd2c05585a
 
     public HopDongViewModel() {
     }
 
-<<<<<<< HEAD
-    public HopDongViewModel(String id, String idnv, String idkh, Date ngayTao, Date ngayHetHan, int tinhTrang) {
-        this.id = id;
-        this.idnv = idnv;
-        this.idkh = idkh;
-        this.ngayTao = ngayTao;
-        this.ngayHetHan = ngayHetHan;
+    public int getTinhTrang() {
+        return tinhTrang;
+    }
+
+    public void setTinhTrang(int tinhTrang) {
         this.tinhTrang = tinhTrang;
-=======
-    public HopDongViewModel(String id, String idnv, String idkh, Date ngaytao, Date ngayhethan, int tinhTrangXe) {
-        this.id = id;
-        this.idnv = idnv;
-        this.idkh = idkh;
-        this.ngaytao = ngaytao;
-        this.ngayhethan = ngayhethan;
-        this.tinhTrangXe = tinhTrangXe;
->>>>>>> d8f254dd8ea954ed9152f2843ee961bd2c05585a
     }
 
     public String getId() {
@@ -78,7 +56,6 @@ public class HopDongViewModel {
         this.idkh = idkh;
     }
 
-<<<<<<< HEAD
     public Date getNgayTao() {
         return ngayTao;
     }
@@ -93,42 +70,6 @@ public class HopDongViewModel {
 
     public void setNgayHetHan(Date ngayHetHan) {
         this.ngayHetHan = ngayHetHan;
-=======
-    public Date getNgaytao() {
-        return ngaytao;
-    }
 
-    public void setNgaytao(Date ngaytao) {
-        this.ngaytao = ngaytao;
     }
-
-    public Date getNgayhethan() {
-        return ngayhethan;
-    }
-
-    public void setNgayhethan(Date ngayhethan) {
-        this.ngayhethan = ngayhethan;
->>>>>>> d8f254dd8ea954ed9152f2843ee961bd2c05585a
-    }
-
-    public int getTinhTrang() {
-        return tinhTrang;
-    }
-
-    public void setTinhTrang(int tinhTrang) {
-        this.tinhTrang = tinhTrang;
-    }
-<<<<<<< HEAD
-    
-=======
-
-    
-    public String hienThiTinhTrang(){
-        if (tinhTrangXe == 0) {
-            return "chưa ký";
-        }else
-            return "đã ký";
-    }
-        
->>>>>>> d8f254dd8ea954ed9152f2843ee961bd2c05585a
 }
