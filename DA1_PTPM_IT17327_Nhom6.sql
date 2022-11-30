040fb167814f2568d66cf0b020576c19a1598d50
Nguyễn Mạnh Hà <109425188+CrowHa@users.noreply.github.com>
2022-11-30 17:49:26 +0700

    Hà - DB có dữ Liệu NV,KH,LX,CTX,VP, PBD,HD

--- DA1_PTPM_IT17327_Nhom6.sql
+++ DA1_PTPM_IT17327_Nhom6.sql
@@ -26,7 +26,6 @@ create table chiTietXe(
 	giathuetrenngay money,
 	mota nvarchar(50)
 )
-alter table 
 
 create table nhanVien(
 	idnv nvarchar(50) primary key,
@@ -39,8 +38,7 @@ create table nhanVien(
 
 
 create table khachHang(
-	id uniqueidentifier primary key,
-	maKH nvarchar(50),
+	maKH nvarchar(50) primary key,
 	ten nvarchar(50),
 	sdt nvarchar(50),
 	gioiTinh bit,
@@ -51,13 +49,26 @@ create table khachHang(
 create table hopDong(
 	id uniqueidentifier primary key,
 	idNv nvarchar(50) foreign key references nhanVien(idnv),
-	idKh uniqueidentifier foreign key references khachHang(id),
+	makh nvarchar(50) foreign key references khachHang(makh),
 	ngayTao date,
 	ngayHetHan date,
 	tinhTrang int,
 	sourceAnh nvarchar(50),
 	mahopdong nvarchar(50)
 )
+insert into hopDong Values(NEWID(),'NV1','KH21','2022-11-30','2022-12-15',1,'','HD1')
+insert into hopDong Values(NEWID(),'NV2','KH11','2022-11-20','2022-12-15',1,'','HD2')
+insert into hopDong Values(NEWID(),'NV1','KH6','2022-11-10','2022-12-15',1,'','HD3')
+insert into hopDong Values(NEWID(),'NV3','KH2','2022-11-12','2022-12-15',1,'','HD4')
+insert into hopDong Values(NEWID(),'NV2','KH30','2022-11-11','2022-12-15',1,'','HD5')
+insert into hopDong Values(NEWID(),'NV1','KH10','2022-11-13','2022-12-15',1,'','HD6')
+insert into hopDong Values(NEWID(),'NV7','KH17','2022-11-11','2022-12-15',1,'','HD7')
+insert into hopDong Values(NEWID(),'NV4','KH29','2022-11-30','2022-12-15',1,'','HD8')
+insert into hopDong Values(NEWID(),'NV5','KH22','2022-11-27','2022-12-15',1,'','HD9')
+insert into hopDong Values(NEWID(),'NV1','KH13','2022-11-23','2022-12-15',1,'','HD10')
+insert into hopDong Values(NEWID(),'NV6','KH4','2022-11-25','2022-12-15',1,'','HD11')
+select * from chiTietXe where tinhTrangXe = 1;
+select * from khachHang
 create table ViPham(
 	mavipham int primary key,
 	ten nvarchar(50),
@@ -65,7 +76,7 @@ create table ViPham(
 )
 create table hoaDonTraXe(
 	id uniqueidentifier primary key,
-	idKh uniqueidentifier foreign key references khachHang(id),
+	makh nvarchar(50) foreign key references khachHang(makh),
 	idHopDong uniqueidentifier foreign key references hopDong(id),
 	ngayTra date,
 	soLuong int,
@@ -97,36 +108,36 @@ insert into nhanVien values('NV7',N'Trần Ngọc Huy','2003-11-04','06236365313
 
 
 
-insert into khachHang values(newid(),'KH1',N'Nguyễn Mạnh Hà','0323423534',0,'01245672','hanmph27977@fpt.edu.vn')
-insert into khachHang values(newid(),'KH2',N'Nguyễn Văn An','0125121451',0,'01153472','mailtest@gmail.com')
-insert into khachHang values(newid(),'KH3',N'Trần Hữu Nam','0125156123',0,'04325642','mailtest@gmail.com')
-insert into khachHang values(newid(),'KH4',N'Nguyễn Hữu Bình','0678967845',0,'01346372','mailtest@gmail.com')
-insert into khachHang values(newid(),'KH5',N'Hoàng Việt Dũng','0456783452',0,'01256312','otosakayuu203@gmail.com')
-insert into khachHang values(newid(),'KH6',N'Hoàng Thùy Chi','0345728956',1,'04574435','dunghvph28012@fpt.edu.vn')
-insert into khachHang values(newid(),'KH7',N'Nguyễn Long Nam','0351674588',0,'04568456','mailtest@gmail.com')
-insert into khachHang values(newid(),'KH8',N'Bùi Thị Kim Anh','0567846756',1,'02367245','mailtest@gmail.com')
-insert into khachHang values(newid(),'KH9',N'Phan Công Văn','0679456312',0,'03467234','mailtest@gmail.com')
-insert into khachHang values(newid(),'KH10',N'Nam Thái Sơn','0456867934',0,'01234624','mailtest@gmail.com')
-insert into khachHang values(newid(),'KH11',N'Hoàng Thùy Linh','0457834535',1,'01234624','mailtest@gmail.com')
-insert into khachHang values(newid(),'KH12',N'Nguyễn Quỳnh Chi','097684254',1,'01234624','mailtest@gmail.com')
-insert into khachHang values(newid(),'KH13',N'Nguyễn Văn An','064567456',0,'03647345','mailtest@gmail.com')
-insert into khachHang values(newid(),'KH14',N'Nguyễn Văn Thái','0345572341',0,'02356234','mailtest@gmail.com')
-insert into khachHang values(newid(),'KH15',N'Nguyễn Nam Kim','0125612452',0,'09678576','mailtest@gmail.com')
-insert into khachHang values(newid(),'KH16',N'Trần Kim Sắc','0123456789',0,'04568256','anhntph28042@fpt.edu.vn')
-insert into khachHang values(newid(),'KH17',N'Lục Văn Dũng','0987654321',0,'05683455','mailtest@gmail.com')
-insert into khachHang values(newid(),'KH18',N'Lê Tuấn Phong','0112233445',0,'02362347','huytnph27916@fpt.edu.vn')
-insert into khachHang values(newid(),'KH19',N'Kiều Hoàng Mai','0556677889',1,'03456734','mailtest@gmail.com')
-insert into khachHang values(newid(),'KH20',N'Phan Quốc Huy','0125756781',0,'03451364','mailtest@gmail.com')
-insert into khachHang values(newid(),'KH21',N'Nguyễn Kim An','0345734513',0,'01235624','mailtest@gmail.com')
-insert into khachHang values(newid(),'KH22',N'Trần Quốc Bảo','0457456524',0,'03467345','mailtest@gmail.com')
-insert into khachHang values(newid(),'KH23',N'Hoàng Mạnh Cường','0346716878',0,'06578567','mailtest@gmail.com')
-insert into khachHang values(newid(),'KH24',N'Nguyễn Thái Bình','01234564832',0,'02367345','mailtest@gmail.com')
-insert into khachHang values(newid(),'KH25',N'Trần Văn Nam','09847894586',0,'04563452','mailtest@gmail.com')
-insert into khachHang values(newid(),'KH26',N'Lê Thị Nga','045674568322',1,'06458865','mailtest@gmail.com')
-insert into khachHang values(newid(),'KH27',N'Nguyễn Việt Anh','0333675875',0,'04567345','mailtest@gmail.com')
-insert into khachHang values(newid(),'KH28',N'Trần Ngọc Huy','0856668563',0,'03456234','huytranisdabest@gmail.com')
-insert into khachHang values(newid(),'KH29',N'Đỗ Thành Cường','04567345734',1,'03457234','mailtest@gmail.com')
-insert into khachHang values(newid(),'KH30',N'Nguyễn Trung Anh','08746584588',0,'01561235','nguyentrunganhnta43@gmail.com')
+insert into khachHang values('KH1',N'Nguyễn Mạnh Hà','0323423534',0,'01245672','hanmph27977@fpt.edu.vn')
+insert into khachHang values('KH2',N'Nguyễn Văn An','0125121451',0,'01153472','mailtest@gmail.com')
+insert into khachHang values('KH3',N'Trần Hữu Nam','0125156123',0,'04325642','mailtest@gmail.com')
+insert into khachHang values('KH4',N'Nguyễn Hữu Bình','0678967845',0,'01346372','mailtest@gmail.com')
+insert into khachHang values('KH5',N'Hoàng Việt Dũng','0456783452',0,'01256312','otosakayuu203@gmail.com')
+insert into khachHang values('KH6',N'Hoàng Thùy Chi','0345728956',1,'04574435','dunghvph28012@fpt.edu.vn')
+insert into khachHang values('KH7',N'Nguyễn Long Nam','0351674588',0,'04568456','mailtest@gmail.com')
+insert into khachHang values('KH8',N'Bùi Thị Kim Anh','0567846756',1,'02367245','mailtest@gmail.com')
+insert into khachHang values('KH9',N'Phan Công Văn','0679456312',0,'03467234','mailtest@gmail.com')
+insert into khachHang values('KH10',N'Nam Thái Sơn','0456867934',0,'01234624','mailtest@gmail.com')
+insert into khachHang values('KH11',N'Hoàng Thùy Linh','0457834535',1,'01234624','mailtest@gmail.com')
+insert into khachHang values('KH12',N'Nguyễn Quỳnh Chi','097684254',1,'01234624','mailtest@gmail.com')
+insert into khachHang values('KH13',N'Nguyễn Văn An','064567456',0,'03647345','mailtest@gmail.com')
+insert into khachHang values('KH14',N'Nguyễn Văn Thái','0345572341',0,'02356234','mailtest@gmail.com')
+insert into khachHang values('KH15',N'Nguyễn Nam Kim','0125612452',0,'09678576','mailtest@gmail.com')
+insert into khachHang values('KH16',N'Trần Kim Sắc','0123456789',0,'04568256','anhntph28042@fpt.edu.vn')
+insert into khachHang values('KH17',N'Lục Văn Dũng','0987654321',0,'05683455','mailtest@gmail.com')
+insert into khachHang values('KH18',N'Lê Tuấn Phong','0112233445',0,'02362347','huytnph27916@fpt.edu.vn')
+insert into khachHang values('KH19',N'Kiều Hoàng Mai','0556677889',1,'03456734','mailtest@gmail.com')
+insert into khachHang values('KH20',N'Phan Quốc Huy','0125756781',0,'03451364','mailtest@gmail.com')
+insert into khachHang values('KH21',N'Nguyễn Kim An','0345734513',0,'01235624','mailtest@gmail.com')
+insert into khachHang values('KH22',N'Trần Quốc Bảo','0457456524',0,'03467345','mailtest@gmail.com')
+insert into khachHang values('KH23',N'Hoàng Mạnh Cường','0346716878',0,'06578567','mailtest@gmail.com')
+insert into khachHang values('KH24',N'Nguyễn Thái Bình','01234564832',0,'02367345','mailtest@gmail.com')
+insert into khachHang values('KH25',N'Trần Văn Nam','09847894586',0,'04563452','mailtest@gmail.com')
+insert into khachHang values('KH26',N'Lê Thị Nga','045674568322',1,'06458865','mailtest@gmail.com')
+insert into khachHang values('KH27',N'Nguyễn Việt Anh','0333675875',0,'04567345','mailtest@gmail.com')
+insert into khachHang values('KH28',N'Trần Ngọc Huy','0856668563',0,'03456234','huytranisdabest@gmail.com')
+insert into khachHang values('KH29',N'Đỗ Thành Cường','04567345734',1,'03457234','mailtest@gmail.com')
+insert into khachHang values('KH30',N'Nguyễn Trung Anh','08746584588',0,'01561235','nguyentrunganhnta43@gmail.com')
 
 
 insert into loaixe values ('LX1', 'Wave Alpha', 12000000, N'Wave', N'Xe số')
